468f6e42fb6a178ce7f493a1eb7b847e6c056819
Anthony Rubat <rubata@F214-11.e-im2ag.ujf-grenoble.fr>
2018-03-21 16:22:43 +0100

    models line and stop

---
+++ app/src/main/java/com/grenoble/miage/metromobilite/model/TransportStop.java
@@ -0,0 +1,60 @@
+package com.grenoble.miage.metromobilite.model;
+
+/**
+ * Created by rubata on 21/03/18.
+ */
+
+public class TransportStop {
+
+    // VARIABLES
+
+    private String code;
+    private String city;
+    private String name;
+    private double lon;
+    private double lat;
+
+    // GETTERS AND SETTERS
+
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getLon() {
+        return lon;
+    }
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+    public double getLat() {
+        return lat;
+    }
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    // CONSTRUCTOR
+
+    public TransportStop(String code, String city, String name, double lon, double lat) {
+        this.code = code;
+        this.city = city;
+        this.name = name;
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+}
